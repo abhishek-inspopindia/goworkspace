3c15c8ab0b03b06bc8b9f1a4e503bf26f85269a7
abhishek-inspopindia <abhishek.tiwari@inspopindia.com>
2019-12-03 23:24:58 +0530

    Molularize the FizzBuzz code

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -6,37 +6,43 @@ import (
 	"strconv"
 )
 
-func main() {
+// global variable declaration
+var c = make(chan string)
+var m = make(map[int]string)
 
+func main() {
 	fizz := 3
 	buzz := 5
+
+	go CreateFizzBuzz(fizz, buzz)
+
+	MapFizzBuzz()
+
+	fmt.Println(m)
+}
+
+// CreateFizzBuzz - uses Channels to generate Key-Value pairs of the Map
+func CreateFizzBuzz(fizz, buzz int) {
 	fizzbuzz := fizz * buzz
-
-	m := make(map[int]string)
-
-	c := make(chan string)
-
-	go func() {
-		for i := 1; i <= 100; i++ {
-			if i%fizzbuzz == 0 {
-				c <- "FizzBuzz"
-			} else if i%fizz == 0 {
-				c <- "Fizz"
-			} else if i%buzz == 0 {
-				c <- "Buzz"
-			} else {
-				c <- strconv.Itoa(i)
-			}
+	for i := 1; i <= 100; i++ {
+		if i%fizzbuzz == 0 {
+			c <- "FizzBuzz"
+		} else if i%fizz == 0 {
+			c <- "Fizz"
+		} else if i%buzz == 0 {
+			c <- "Buzz"
+		} else {
+			c <- strconv.Itoa(i)
 		}
-		close(c)
-	}()
+	}
+	close(c)
+}
 
+// MapFizzBuzz - maps the channel values to the global map variable
+func MapFizzBuzz() {
 	cnt := 1
 	for n := range c {
 		m[cnt] = n
 		cnt++
-		//fmt.Println(n)
 	}
-
-	defer fmt.Println(m)
 }
