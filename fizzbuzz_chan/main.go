9bf25ba56e2494388a30c02bacd26110b3ecc95f
abhishek-inspopindia <abhishek.tiwari@inspopindia.com>
2019-12-05 19:12:47 +0530

    Created new Package for FizzBizz using Channels and Map

---
+++ fizzbuzz_chan/main.go
@@ -0,0 +1,17 @@
+// Package main contains the modules to create a FizzBuzz using Map
+package main
+
+import (
+	"fmt"
+
+	fizzbuzzchan "github.com/abhishek-inspopindia/goworkspace/fizzbuzz_chan/pkg_fizzbuzz_chan"
+)
+
+func main() {
+	fizz := 3
+	buzz := 5
+
+	fizzbuzzchan.FizzBuzz(fizz, buzz)
+
+	fmt.Println(m)
+}
---
+++ fizzbuzz_chan/pkg_fizzbuzz_chan/fizzbuzz_chan.go
@@ -0,0 +1,44 @@
+// Package fizzbuzzchan contains the modules to create a FizzBuzz using Channels and Map
+package fizzbuzzchan
+
+import (
+	"strconv"
+)
+
+// global variable declaration
+var c = make(chan string)
+var m = make(map[int]string)
+
+// FizzBuzz - uses Channels and Maps to create FizzBuzz
+func FizzBuzz(fizz, buzz int) map[int]string {
+	go CreateFizzBuzz(fizz, buzz, c)
+	MapFizzBuzz(m, c)
+
+	return m
+}
+
+// CreateFizzBuzz - creates FizzBuzz and insert values into the Channel
+func CreateFizzBuzz(fizz, buzz int, c chan string) {
+	fizzbuzz := fizz * buzz
+	for i := 1; i <= 100; i++ {
+		if i%fizzbuzz == 0 {
+			c <- "FizzBuzz"
+		} else if i%fizz == 0 {
+			c <- "Fizz"
+		} else if i%buzz == 0 {
+			c <- "Buzz"
+		} else {
+			c <- strconv.Itoa(i)
+		}
+	}
+	close(c)
+}
+
+// MapFizzBuzz - maps the channel values to the global map variable
+func MapFizzBuzz(m map[int]string, c chan string) {
+	cnt := 1
+	for n := range c {
+		m[cnt] = n
+		cnt++
+	}
+}
